931e97628fa6276bf2f24c4ae03d7d2224679ca0
Maciej Buczyński <najkesekofficial@gmail.com>
2024-03-21 14:55:38 +0100

    addedTexture

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// const { shadowMapFragment } = require("@babylonjs/core/Shaders/ShadersInclude/shadowMapFragment");
+
+// const { RotationXBlock } = require("@babylonjs/core");
+
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
         const engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true }); // Generate the BABYLON 3D engine
         const createScene = function () {
@@ -9,13 +13,16 @@ const canvas = document.getElementById("renderCanvas"); // Get the canvas elemen
                 camera.attachControl(canvas, true,);
                 const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
                 // Dim the light a small amount - 0 to 1
-                light.intensity = 0.0;
+                light.intensity = 0.2;
 
 
                 // Light Sphere
-                var light0 = new BABYLON.SpotLight("Omni0", new BABYLON.Vector3(0, 10, 0), scene);
-                light0.position = new BABYLON.Vector3(-5, 7, -6)
-                light0.intensity = 1;
+                var light0 = new BABYLON.SpotLight("Omni0", new BABYLON.Vector3(0, 0, 0), new BABYLON.Vector3(1, -1, 1), 8, 10, scene);
+                light0.position = new BABYLON.Vector3(-6, 7, -6)
+                light0.intensity = 1.5;
+                light0.shadowEnabled = true;
+                light0.shadowMinZ = 1;
+                light0.shadowMaxZ = 12.5;
 
                 var lightSphere0 = BABYLON.Mesh.CreateSphere("Sphere0", 16, 0.5, scene);
                 lightSphere0.position = light0.position;
@@ -26,6 +33,8 @@ const canvas = document.getElementById("renderCanvas"); // Get the canvas elemen
 
                 
                 // var sphere = BABYLON.Mesh.CreateSphere("Sphere", 16, 3, scene);
+                
+
                 
                 
                 // Sphere material
@@ -41,12 +50,47 @@ const canvas = document.getElementById("renderCanvas"); // Get the canvas elemen
                 const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 25, height: 25}, scene);
                 ground.position = new BABYLON.Vector3(0, -3, 0);
 
+                // Box creation
                 const box = BABYLON.MeshBuilder.CreateBox('box', {width: 5, height: 5, depth:5}, scene);
 
-                const shadowGenerator = new BABYLON.ShadowGenerator(512, light0);
-                shadowGenerator.getShadowMap().renderList.push(box);
-                shadowGenerator.useContactHardeningShadow = true;
-                shadowGenerator.contactHardeningLightSizeUVRatio = 0.0075;
+                const boxMat = new BABYLON.StandardMaterial("boxMat", scene)
+                boxMat.diffuseTexture = new BABYLON.Texture("textures/speckles.jpg", scene)
+                
+                box.material = boxMat;
+
+                const framerate = 60;
+                //Animations 
+                const anim1 = new BABYLON.Animation("anim1", "rotation.y", framerate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+                
+                const keyframes = []; 
+                keyframes.push({
+                    frame: 0,
+                    value: 0
+                });
+
+                keyframes.push({
+                        frame: framerate,
+                        value: 2 * framerate
+                })
+            
+                keyframes.push({
+                    frame: 60,
+                    value: 9000
+                });
+
+                anim1.setKeys(keyframes);
+                box.animations = [];
+                box.animations.push(anim1);
+
+
+                const shadowGenerator = new BABYLON.ShadowGenerator(2048, light0);
+                shadowGenerator.useBlurCloseExponentialShadowMap = true; 
+                box.receiveShadows = true;
+                shadowGenerator.addShadowCaster(box);
+                // const shadowGenerator = new BABYLON.ShadowGenerator(2048, light0);
+                // shadowGenerator.getShadowMap().renderList.push(box);
+                // shadowGenerator.useContactHardeningShadow = true;
+                // shadowGenerator.contactHardeningLightSizeUVRatio = 0.075;
                 // shadowGenerator.useBlurExponentialShadowMap = true;
                 // shadowGenerator.useKernelBlur = true;
                 // shadowGenerator.blurKernel = 64;
@@ -54,7 +98,7 @@ const canvas = document.getElementById("renderCanvas"); // Get the canvas elemen
                 // light0.shadowEnabled = true;
                 ground.receiveShadows = true;
                 // box.receiveShadows = true;
-
+                scene.beginAnimation(anim1, 0, 60, true);
                 return scene;
         };
         const scene = createScene(); //Call the createScene function
